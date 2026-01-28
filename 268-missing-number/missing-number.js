6ac4e39c9e7f60715d896df6e3d112882d233168
MUHAMMED-ANAS.K <147187303+Muhammedanas-kunnath@users.noreply.github.com>
2026-01-28 11:09:56 +0530

    Time: 218 ms (5.04%) | Memory: 55.5 MB (93.65%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,14 @@
+var missingNumber = function(nums) {
+  let i;
+  store=""
+  
+  
+  
+    for(i=0; i<=nums.length;i++){
+      nums[i]
+      
+      if(!nums.includes(i)){
+        return i
+        }
+    }
+}
